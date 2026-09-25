2955ebf3d5d1c05a953e67e772e5e920d8531dd5
RTLiang <liangrtsmail@gmail.com>
2026-09-25 10:29:47 +0800

    ci: sync patched Helium and release personal arm64 builds

--- .github/scripts/github_prepare_artifacts.sh
+++ .github/scripts/github_prepare_artifacts.sh
@@ -19,14 +19,14 @@ if [ -f "$_root_dir/build_finished_$_target_cpu.log" ]; then
 
   xattr -cs out/Default/Helium.app
 
-  # Prepar the certificate for app signing
-  echo $MACOS_CERTIFICATE | base64 --decode > "$TMPDIR/certificate.p12"
-
-  security create-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
-  security default-keychain -s build.keychain
-  security unlock-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
-  security import "$TMPDIR/certificate.p12" -k build.keychain -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign
-  security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+  if [ -n "${MACOS_CERTIFICATE_NAME:-}" ]; then
+    echo "$MACOS_CERTIFICATE" | base64 --decode > "$TMPDIR/certificate.p12"
+    security create-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+    security default-keychain -s build.keychain
+    security unlock-keychain -p "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+    security import "$TMPDIR/certificate.p12" -k build.keychain -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign
+    security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$MACOS_CI_KEYCHAIN_PWD" build.keychain
+  fi
 
   if ! [ -z "${PROD_MACOS_SPECIAL_ENTITLEMENTS_PROFILE_B64:-}" ]; then
     export PROD_MACOS_SPECIAL_ENTITLEMENTS_PROFILE_PATH=$(mktemp)
@@ -62,9 +62,11 @@ if [ -f "$_root_dir/build_finished_$_target_cpu.log" ]; then
     DELTA_ARG="--arm"
   fi
 
-  PATH="$_src_dir/out/Default:$PATH" python3 "$_root_dir/devutils/generate_sparkle_deltas.py" \
-    "$DELTA_ARG" "./release_asset/$_file_name" \
-    --out ./release_asset
+  if [ -n "${MACOS_CERTIFICATE_NAME:-}" ]; then
+    PATH="$_src_dir/out/Default:$PATH" python3 "$_root_dir/devutils/generate_sparkle_deltas.py" \
+      "$DELTA_ARG" "./release_asset/$_file_name" \
+      --out ./release_asset
+  fi
 
   {
     echo 'deltas<<EOF'
